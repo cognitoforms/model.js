9476961f5b73261579847fbc293b07cb7972d7f3
Danni <danielletalley@outlook.com>
2020-05-19 11:22:05 -0400

    Update range-rule.ts
    Standardize the datetime based on the format specifier so that it can be used appropriately for comparisons. When comparing time values, the date will be set to Jan 1 1970. When comparing date values, the time will be set to 12AM.
    https://cognitoforms.visualstudio.com/Cognito%20Forms/_workitems/edit/12040/

--- src/range-rule.ts
+++ src/range-rule.ts
@@ -2,6 +2,7 @@ import { ValidationRule, ValidationRuleOptions } from "./validation-rule";
 import { Property$format } from "./property";
 import { Entity } from "./entity";
 import { Type } from "./type";
+import { Format } from "./format";
 
 /**
  * A rule that validates that a property value is within a specific range
@@ -16,12 +17,19 @@ export class RangeRule extends ValidationRule {
 		// ensure the rule name is specified
 		options.name = options.name || "Range";
 
-		options.message = function(this: Entity): string {
-			var range: { min?: any; max?: any } = {};
+		options.message = function(this: Entity): string {			
+			let format = options.property.format;		
+			var val = RangeRule.standardize(options.property.value(this), format);
 
+			if (val == null) {
+				return null;
+			}	
+			
+			var range: { min?: any; max?: any } = {};
+				
 			if (options.min && options.min instanceof Function) {
 				try {
-					range.min = options.min.call(this);
+					range.min = RangeRule.standardize(options.min.call(this), format);					
 				}
 				catch (e) {
 					// Silently ignore min errors
@@ -30,20 +38,14 @@ export class RangeRule extends ValidationRule {
 
 			if (options.max && options.max instanceof Function) {
 				try {
-					range.max = options.max.call(this);
+					range.max = RangeRule.standardize(options.max.call(this), format);
 				}
 				catch (e) {
 					// Silently ignore max errors
 				}
 			}
 
-			var val = options.property.value(this);
-
-			if (val == null) {
-				return null;
-			}
-
-			if ((range.min == null || val >= range.min) && (range.max == null || val <= range.max)) {
+			if ((range.min == null || val >= range.min) && (range.max == null || val <= range.max)) {				
 				// Value is within range
 				return null;
 			}
@@ -71,6 +73,43 @@ export class RangeRule extends ValidationRule {
 	// get the string representation of the rule
 	toString(): string {
 		return `${this.property.containingType.fullName}.${this.property.name} in range, min: , max: `;
+	}
+
+	// January 1st, 1970 at 12AM
+	private static JAN_01_1970 = new Date(18000000);
+
+	// Standardize the provided value based on the format specifier
+	// so that it can be used appropriately for comparisons
+	private static standardize(val: any, format: Format<any>) : any {
+		if (!val && val !== false)
+			return val;
+
+		if (val.constructor.name === "Date") {
+			if (format.specifier === "t")
+				val = RangeRule.standardizeDate(val);
+			else if (format.specifier === "d")
+				val = RangeRule.standardizeTime(val);
+		}
+
+		return val;
+	}
+	
+	// Set the date of the dateTime to January 1st, 1970
+	private static standardizeDate(dateTime: Date): Date {
+		return RangeRule._standardizeDate(dateTime, RangeRule.JAN_01_1970);
+	}
+
+	// Set the date of the dateTime to the supplied standardized date
+	private static _standardizeDate(dateTime: Date, standard: Date): Date {			
+		dateTime.setMonth(standard.getMonth());
+		dateTime.setDate(standard.getDate());
+		dateTime.setFullYear(standard.getFullYear());
+		return dateTime;
+	}
+
+	// Set the time of the dateTime to 12AM	
+	private static standardizeTime(dateTime: Date): Date {
+		return RangeRule._standardizeDate(RangeRule.JAN_01_1970, dateTime);
 	}
 }
 
