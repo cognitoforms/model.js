5cc3eceb93626a5fab6fa15773dcebdafa3d771a
Danni <danielletalley@outlook.com>
2020-05-20 14:11:35 -0400

    Access to ranges in rules
    - Use new copy of static date
    - Set the range on the rule object before executing the range rule
      > It will be used in the component off of the range rule

--- src/condition-rule.ts
+++ src/condition-rule.ts
@@ -18,6 +18,8 @@ export class ConditionRule extends Rule {
 	// The condition type to raise when asserted
 	conditionType: ConditionType;
 
+	preExecute?: (this: ConditionRule, entity: Entity) => void;
+
 	/**
 	 * Creates a rule that asserts a condition based on a predicate
 	 * @param rootType The model type the rule is for
@@ -32,6 +34,9 @@ export class ConditionRule extends Rule {
 	
 		// assertion function
 		this.assert = options.assert;
+
+		// pre-execute
+		this.preExecute = options.preExecute;
 
 		// message
 		this.message = options.message;
@@ -50,6 +55,10 @@ export class ConditionRule extends Rule {
 	execute(entity: Entity): void {
 		let assert: boolean;
 		let message: string;
+
+		if(this.preExecute) {
+			this.preExecute(entity);
+		}
 
 		if (this.assert) {
 			// If an assert function is defined, then use it to determine whether to attach a condition
@@ -95,4 +104,5 @@ export interface ConditionRuleOptions extends RuleOptions {
 	// the optional array of condition type sets to associate the condition with
 	sets?: ConditionTypeSet[];
 
+	preExecute?: (this: ConditionRule, entity: Entity) => void;
 }
