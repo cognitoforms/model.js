4f49b41d2de93bd3d77d6028b88fe6af8c24faa5
pkellz <patrickscott2013@gmail.com>
2021-02-12 08:24:24 -0500

    Add context/comments

--- src/property-chain.unit.ts
+++ src/property-chain.unit.ts
@@ -11,8 +11,22 @@ function createModel(options): Promise<Model> {
 	});
 }
 
+/*
+    Context
+    Description from Bug 18635:
+    https://cognitoforms.visualstudio.com/Cognito%20Forms/_workitems/edit/18635
+
+    "A customer on Zendesk had noticed considerable delay before their public form can be interacted with on load. We have noticed anywhere from a ~4s-10s delay when reproducing. The form has a lookup field that references a form with > 7000 entries. This, in and of itself, shouldn't be a big issue. But the field also has 9 cascading filters. We believe this combination is the main contributing factor of the slowdown.
+
+    It appears the bulk of the time is spent looping through lookup entry index instances when initializing lookups. I think this can be optimized in the framework to eliminate this unnecessary work, which would reduce the delay considerable, if not altogether. This would have to be reproduced and the fix tested to prove that it would actually address the problem. Also, the same issue affects Vue forms, so it should at least be fixed there."
+
+    The fix for this is a slight optimization for changes to the first property in a property chain. In the fix, we won't unnecessarily loop over pooled entities to see if they are linked to the changed entity via a null property, because testConnection() would only return true if they are the same entity.
+*/
+
 describe("PropertyChain", () => {
-	it("does something", async () => {
+	it("Doesn't call PropertyChain.testConnection() when not needed", async () => {
+		// To simulate the lookup scenario from the bug above, we create a model that has some type (Person).
+		// This type has multiple properties (GroupName, GroupId, GroupCountry etc...) that references back to one shared object's (Group) properties
 		const model = await createModel({
 			Person: {
 				Name: {
@@ -49,49 +63,51 @@ describe("PropertyChain", () => {
 					  dependsOn: "Group.Language",
 					  function() { return this.Group.Language; }
 					}
-				},
-				GroupXYZ: {
-					type: String,
-					get: {
-					  dependsOn: "Group.XYZ",
-					  function() { return this.Group.XYZ; }
-					}
-				},
-				GroupABC: {
-					type: String,
-					get: {
-					  dependsOn: "Group.ABC",
-					  function() { return this.Group.XYZ; }
-					}
 				}
-
 			},
 			Group: {
 				Name: String,
 				Id: String,
 				Country: String,
-				Language: String,
-				ABC: String,
-				XYZ: String
+				Language: String
 			}
 		});
 
 		const Person = model.getJsType("Person");
-		const chain = (Person.meta as Type).getPath("Group.Name");
 
+		// Create multiple Persons (will just sit in memory)
 		for (let i = 0; i < 1000; i++) {
-			const bryan = new Person({ Name: "Bryan" });
+			const person = new Person({ Name: "Dude Man" });
 		}
 
+		// Create new Group
 		const Group = model.getJsType("Group");
-		const group = new Group({ Name: "Patrick's Group", Id: "XDA-1V9-AM3", Country: "US", Language: "English", XYZ: "XYZ", ABC: "SBC" });
+		const group = new Group({
+			Name: "Test Group",
+			Id: "XDA-1V9-AM3",
+			Country: "US",
+			Language: "English"
+		});
 
+		// Spy on testConnection()
 		const testConnectionSpy = jest.spyOn(PropertyChain.prototype, "testConnection");
 
-		for (let p of (Person.meta as Type).known()) {
-			p.Group = group;
-		}
+		/*
+            For all known Persons, initially assign their Group to be the group constructed above
 
+            Before the fix, this assignment would will trigger recalculations for all of the properties on the Person's container object, which would unnecessarily loop over pooled entities to see if they are linked to the changed entity via a null property. This is where the slowdown comes into play.
+
+            With the fix, we would prevent unnecessarily looping over said entities.
+        */
+		for (let p of (Person.meta as Type).known())
+			p.Group = group;
+
+		//	Assure that testConnection() is not called (we are not unnecessarily looping) on initial assignment
 		expect(testConnectionSpy).not.toBeCalled();
+
+		/*
+            Prior to fix, testConnection() would be called 4,000,000 times
+		    4 (number of dependent properties) * 1000 (Persons in memory) * 1000 (iterations)
+        */
 	});
 });
