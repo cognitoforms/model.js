7f67d3cb66de941488c375191d4fcab36dc0c3f1
Taylor Kimmett <taylor@cognitoforms.com>
2021-03-31 17:54:41 -0400

    use UTC dates for normalize tests

--- src/model.unit.ts
+++ src/model.unit.ts
@@ -3,13 +3,13 @@ import { Model, normalize, ModelConfiguration } from "./model";
 describe("normalize", () => {
 	it("returns the time portion of the given date if the format is 't'", async () => {
 		const ts = new Date(1597757877175);
-		expect(ts).toEqual(new Date(2020, 7, 18, 9, 37, 57, 175));
+		expect(ts).toEqual(new Date(Date.UTC(2020, 7, 18, 13, 37, 57, 175)));
 		expect(normalize(ts, "t")).toEqual(new Date(1970, 0, 1, 9, 37, 57, 175));
 	});
 
 	it("returns the day portion of the given date if the format is 'd'", async () => {
 		const ts = new Date(1597757877175);
-		expect(ts).toEqual(new Date(2020, 7, 18, 9, 37, 57, 175));
+		expect(ts).toEqual(new Date(Date.UTC(2020, 7, 18, 13, 37, 57, 175)));
 		expect(normalize(ts, "d")).toEqual(new Date(2020, 7, 18));
 	});
 });
