8cb0e8d556f743e3a93a6048a2f5120fe3fef2ba
Taylor Kimmett <taylor@cognitoforms.com>
2022-11-28 12:35:36 -0500

    code comment

--- src/property.ts
+++ src/property.ts
@@ -647,6 +647,7 @@ export interface PropertyOptions {
 	/** An optional constant default value, or a function or dependency function object that calculates the default value of this property. */
 	default?: PropertyValueFunction<any> | PropertyValueFunctionAndOptions<any> | Value | Value[];
 
+	/** A function used to obtain the value with which to initialize this property. The function is called only once, and is not reactive as a rule would be. */
 	init?: PropertyValueFunction<any>;
 
 	/** An optional constant default value, or a function or dependency function object that calculates the default value of this property. */
