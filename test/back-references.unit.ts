31706e40c37d5f8a02457eeca9cd3c21cc61b039
Bryan Matthews <bryan@cognitoforms.com>
2023-01-03 11:47:00 -0500

    Move test helper functions

--- test/back-references.unit.ts
+++ test/back-references.unit.ts
@@ -54,35 +54,35 @@ class InitializeBackReferencesConverter extends PropertyConverter {
 	}
 }
 
+function propagateRootProperty(parent: Entity, child: Entity, rootPropertyName: string = "Root") {
+	// In case the parent's Root property is not yet set, propagate when it is set
+	if (!parent[rootPropertyName])
+		parent.meta.type.getProperty(rootPropertyName).changed.subscribeOne(e => (child[rootPropertyName] = e.newValue));
+	else
+		child[rootPropertyName] = parent[rootPropertyName];
+}
+
+function setBackReferenceProperties(parent: Entity, child: Entity, rootPropertyName: string = "Root", parentPropertyName: string = "Parent") {
+	if (parentPropertyName in child) {
+		child[parentPropertyName] = parent;
+		propagateRootProperty(parent, child, rootPropertyName);
+	}
+	else
+		child[rootPropertyName] = parent;
+}
+
+function ensureChildProperties(parent: Entity, propertyName: string, rootPropertyName: string = "Root", parentPropertyName: string = "Parent"): void {
+	const value = parent.get(propertyName);
+	if (Array.isArray(value)) {
+		value.forEach(item => setBackReferenceProperties(parent, item, rootPropertyName, parentPropertyName));
+	}
+	else if (value) {
+		setBackReferenceProperties(parent, value, rootPropertyName, parentPropertyName);
+	}
+}
+
 describe("Back-reference properties", () => {
 	describe("from a reference property", () => {
-		function propagateRootProperty(parent: Entity, child: Entity, rootPropertyName: string = "Root") {
-			// In case the parent's Root property is not yet set, propagate when it is set
-			if (!parent[rootPropertyName])
-				parent.meta.type.getProperty(rootPropertyName).changed.subscribeOne(e => (child[rootPropertyName] = e.newValue));
-			else
-				child[rootPropertyName] = parent[rootPropertyName];
-		}
-
-		function setBackReferenceProperties(parent: Entity, child: Entity, rootPropertyName: string = "Root", parentPropertyName: string = "Parent") {
-			if (parentPropertyName in child) {
-				child[parentPropertyName] = parent;
-				propagateRootProperty(parent, child, rootPropertyName);
-			}
-			else
-				child[rootPropertyName] = parent;
-		}
-
-		function ensureChildProperties(parent: Entity, propertyName: string, rootPropertyName: string = "Root", parentPropertyName: string = "Parent"): void {
-			const value = parent.get(propertyName);
-			if (Array.isArray(value)) {
-				value.forEach(item => setBackReferenceProperties(parent, item, rootPropertyName, parentPropertyName));
-			}
-			else if (value) {
-				setBackReferenceProperties(parent, value, rootPropertyName, parentPropertyName);
-			}
-		}
-
 		let refPropModel: Model;
 		beforeEach(() => {
 			refPropModel = new Model({
