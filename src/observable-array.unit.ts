bf5c273828fa72b0990a005b390a9ddccf4cd760
Bryan Matthews <bryan@cognitoforms.com>
2023-05-15 18:24:30 -0400

    Fix no op test

--- src/observable-array.unit.ts
+++ src/observable-array.unit.ts
@@ -4,6 +4,7 @@ describe("ObservableArray", () => {
 	it("should not publish change event for no changes", () => {
 		const arr = ObservableArray.create([]);
 		const changeHandler = jest.fn();
+		arr.changed.subscribe(changeHandler);
 		arr.batchUpdate(() => updateArray(arr, []));
 		expect(changeHandler).not.toBeCalled();
 	});
