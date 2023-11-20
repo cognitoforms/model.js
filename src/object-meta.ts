b335440174a5e5716707ba17aaf6f99e4779293f
Bryan Matthews <bryan@cognitoforms.com>
2023-11-20 08:54:39 -0500

    Use defineProperty to for entity and object meta to make non-enumarble and non-configurable properties

--- src/object-meta.ts
+++ src/object-meta.ts
@@ -18,11 +18,12 @@ export class ObjectMeta {
 	conditions: ObservableArray<ConditionTarget>;
 
 	constructor(type: Type, entity: Entity, id: string, isNew: boolean) {
-		this.type = type;
-		this.entity = entity;
-		this.id = id;
-		this.isNew = isNew;
-		this.conditions = ObservableArray.create<ConditionTarget>();
+		Object.defineProperty(this, "type", { enumerable: true, configurable: false, writable: false, value: type });
+		Object.defineProperty(this, "entity", { enumerable: true, configurable: false, writable: false, value: entity });
+		Object.defineProperty(this, "id", { enumerable: true, configurable: false, writable: true, value: id });
+		Object.defineProperty(this, "isNew", { enumerable: true, configurable: false, writable: true, value: isNew });
+		Object.defineProperty(this, "conditions", { enumerable: true, configurable: false, writable: true, value: ObservableArray.create<ConditionTarget>() });
+		Object.defineProperty(this, "__pendingInvocation__", { enumerable: false, configurable: false, writable: false, value: [] });
 	}
 
 	/**
